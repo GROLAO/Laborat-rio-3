6682463af0dc4e85d79292e668368b8c243551eb
Gabriel Grola <57289346+GROLAO@users.noreply.github.com>
2025-09-24 12:26:15 -0300

    Update Dump20210508.sql

--- Sistema-Loja-API/database/Dump20210508.sql
+++ Sistema-Loja-API/database/Dump20210508.sql
@@ -1,27 +1,5 @@
--- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
---
--- Host: localhost    Database: faixas
--- ------------------------------------------------------
--- Server version	8.0.22
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `cliente`
---
-
 DROP TABLE IF EXISTS `cliente`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `cliente` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
@@ -36,25 +14,17 @@ CREATE TABLE `cliente` (
   `email` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `cliente`
---
 
 LOCK TABLES `cliente` WRITE;
-/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+
 INSERT INTO `cliente` VALUES (1,'Camila Bontempo','11322266603','Av dos Curios',501,'Bloco 18 Ap 403','Pontal','38055100','Uberaba','99460013','camila.frrr1@hotmail.com');
-/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `faixa`
---
 
 DROP TABLE IF EXISTS `faixa`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `faixa` (
   `id` int NOT NULL AUTO_INCREMENT,
   `altura` decimal(6,2) NOT NULL,
@@ -66,47 +36,20 @@ CREATE TABLE `faixa` (
   `pedido` int DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `faixa`
---
-
 LOCK TABLES `faixa` WRITE;
-/*!40000 ALTER TABLE `faixa` DISABLE KEYS */;
+
 INSERT INTO `faixa` VALUES (1,50.00,150.00,'Arthur é chato','green','black',60.00,1);
-/*!40000 ALTER TABLE `faixa` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `hibernate_sequence`
---
-
 DROP TABLE IF EXISTS `hibernate_sequence`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `hibernate_sequence` (
   `next_val` bigint DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `hibernate_sequence`
---
-
 LOCK TABLES `hibernate_sequence` WRITE;
-/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
 INSERT INTO `hibernate_sequence` VALUES (56);
-/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `pedido`
---
-
 DROP TABLE IF EXISTS `pedido`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pedido` (
   `id` int NOT NULL AUTO_INCREMENT,
   `data` datetime NOT NULL,
